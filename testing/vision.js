a5aab25fc39c6cbd58c497a1185c100e12d1f672
Melissa Wen <wenm@usc.edu>
2017-09-16 14:51:28 -0400

    ignore testing folder

--- testing/vision.js
+++ testing/vision.js
@@ -8,25 +8,39 @@
 
 <script type="text/javascript">
     function processImage() {
-        var subscriptionKey = "3bce0d31c44741d988ef428c8577ec9f";
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the subscriptionKey string value with your valid subscription key.
+        var subscriptionKey = "13hc77781f7e4b19b5fcdd72a8df7156";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
         var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
 
-        // Parameters
+        // Request parameters.
         var params = {
             "returnFaceId": "true",
             "returnFaceLandmarks": "false",
             "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
         };
 
-        // Display image
+        // Display the image.
         var sourceImageUrl = document.getElementById("inputImage").value;
         document.querySelector("#sourceImage").src = sourceImageUrl;
 
-        // Perform the REST API call
+        // Perform the REST API call.
         $.ajax({
             url: uriBase + "?" + $.param(params),
 
-            // Request headers
+            // Request headers.
             beforeSend: function(xhrObj){
                 xhrObj.setRequestHeader("Content-Type","application/json");
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
@@ -34,12 +48,12 @@
 
             type: "POST",
 
-            // Request body
+            // Request body.
             data: '{"url": ' + '"' + sourceImageUrl + '"}',
         })
 
         .done(function(data) {
-            // Show formatted JSON on webpage
+            // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
         })
 
@@ -53,7 +67,10 @@
     };
 </script>
 
-Image to analyze: <input type="text" name="inputImage" id="inputImage"/>
+<h1>Detect Faces:</h1>
+Enter the URL to an image that includes a face or faces, then click the <strong>Analyze face</strong> button.
+<br><br>
+Image to analyze: <input type="text" name="inputImage" id="inputImage" value="https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg" />
 <button onclick="processImage()">Analyze face</button>
 <br><br>
 <div id="wrapper" style="width:1020px; display:table;">
